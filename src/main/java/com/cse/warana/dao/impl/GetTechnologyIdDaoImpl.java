f159e5b445e6391b5682d37895e5227446a84fd8
wadsashika <wadsashika@gmail.com>
2015-02-09 01:02:55 +0530

    Measuring score by matching technologies part added

--- src/main/java/com/cse/warana/dao/impl/GetTechnologyIdDaoImpl.java
+++ src/main/java/com/cse/warana/dao/impl/GetTechnologyIdDaoImpl.java
@@ -1,17 +1,17 @@
 package com.cse.warana.dao.impl;
 
 import com.cse.warana.dao.GetTechnologyIdDao;
-import com.cse.warana.dto.ResumesToProcessDto;
 import com.cse.warana.utility.infoHolders.Technology;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.simple.ParameterizedRowMapper;
 import org.springframework.stereotype.Repository;
 
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Nadeeshaan on 1/2/2015.
@@ -26,9 +26,9 @@ public class GetTechnologyIdDaoImpl extends BaseJDBCDaoImpl implements GetTechno
         StringBuilder query = new StringBuilder("");
         StringBuilder techList = new StringBuilder("");
 
-        for (int a = 0; a < technologies.size(); a++){
+        for (int a = 0; a < technologies.size(); a++) {
             techList.append(("'" + technologies.get(a).getName() + "'"));
-            if (a!=technologies.size()-1){
+            if (a != technologies.size() - 1) {
                 techList.append(",");
             }
         }
@@ -43,20 +43,19 @@ public class GetTechnologyIdDaoImpl extends BaseJDBCDaoImpl implements GetTechno
             public String mapRow(ResultSet resultSet, int i) throws SQLException {
                 String id = "";
                 id = resultSet.getString("id");
-                System.out.println(id+"//////////////////////////////////////////////////////////////");
+                System.out.println(id + "//////////////////////////////////////////////////////////////");
                 return id;
             }
         };
 
-        List<String> techIds=null;
-        HashMap<String,String> techMap=new HashMap<>();
+        List<String> techIds = null;
+        HashMap<String, String> techMap = new HashMap<>();
 
         try {
-            techIds = getNamedParameterJdbcTemplate().query(query.toString(),mapper);
+            techIds = getNamedParameterJdbcTemplate().query(query.toString(), mapper);
 
 
-        }
-        catch (NullPointerException e){
+        } catch (NullPointerException e) {
             System.out.println("Null Pointer Exception");
         }
 
@@ -64,13 +63,14 @@ public class GetTechnologyIdDaoImpl extends BaseJDBCDaoImpl implements GetTechno
     }
 
     @Override
+    @TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
     public Map<String, Long> getTechnologyIdMap(List<Technology> technologies) {
         StringBuilder query = new StringBuilder("");
         StringBuilder techList = new StringBuilder("");
 
-        for (int a = 0; a < technologies.size(); a++){
+        for (int a = 0; a < technologies.size(); a++) {
             techList.append(("'" + technologies.get(a).getName() + "'"));
-            if (a!=technologies.size()-1){
+            if (a != technologies.size() - 1) {
                 techList.append(",");
             }
         }
@@ -84,18 +84,17 @@ public class GetTechnologyIdDaoImpl extends BaseJDBCDaoImpl implements GetTechno
             @Override
             public String mapRow(ResultSet resultSet, int i) throws SQLException {
                 String idNameStr = "";
-                idNameStr = resultSet.getString("id") +"," + resultSet.getString("technology").toLowerCase();
+                idNameStr = resultSet.getString("id") + "," + resultSet.getString("technology").toLowerCase();
                 return idNameStr;
             }
         };
 
-        List<String> techIds=null;
+        List<String> techIds = null;
 
         try {
-            techIds = getNamedParameterJdbcTemplate().query(query.toString(),mapper);
+            techIds = getNamedParameterJdbcTemplate().query(query.toString(), mapper);
             System.out.println(techIds.size());
-        }
-        catch (NullPointerException e){
+        } catch (NullPointerException e) {
             System.out.println("Null Pointer Exception");
         }
 
@@ -108,5 +107,67 @@ public class GetTechnologyIdDaoImpl extends BaseJDBCDaoImpl implements GetTechno
 
 
         return techIdMap;
+    }
+
+    @Override
+    @TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
+    public Map<Integer, Double> getCompanyTechnologyScoreMap(Long candidateId) {
+        StringBuilder query = new StringBuilder("");
+        Map<Integer, Double> result = new HashMap<>();
+
+        query.append("SELECT merged.technology_id, merged.score \n");
+        query.append("FROM ( \n");
+        query.append("SELECT comt.technology_id, comt.score, ct.candidate_id \n");
+        query.append("FROM candidate_technology ct \n");
+        query.append("INNER JOIN company_technology comt ON ct.technology_id = comt.technology_id \n");
+        query.append(") merged \n");
+        query.append("WHERE merged.candidate_id = '" + candidateId+"'");
+
+        RowMapper<String> mapper = new RowMapper<String>() {
+            @Override
+            public String mapRow(ResultSet resultSet, int i) throws SQLException {
+                String idNameStr = "";
+                idNameStr = resultSet.getString("technology_id") + "," + resultSet.getString("score");
+                return idNameStr;
+            }
+        };
+
+        List<String> resultSet = getNamedParameterJdbcTemplate().query(query.toString(), mapper);
+
+        for (int i = 0; i < resultSet.size(); i++) {
+            String[] temp = resultSet.get(i).split(",");
+            result.put(Integer.parseInt(temp[0].trim()), Double.parseDouble(temp[1].trim()));
+        }
+
+        return result;
+    }
+
+    @Override
+    @TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
+    public Map<Integer, Double> getCandidateTechnologyScoreMap(Long candidateId) {
+        StringBuilder query = new StringBuilder("");
+        Map<Integer, Double> result = new HashMap<>();
+
+        query.append("SELECT ct.technology_id, ct.percentage \n");
+        query.append("FROM candidate_technology ct \n");
+        query.append("WHERE ct.candidate_id = '" + candidateId+"'");
+
+        RowMapper<String> mapper = new RowMapper<String>() {
+            @Override
+            public String mapRow(ResultSet resultSet, int i) throws SQLException {
+                String idNameStr = "";
+                idNameStr = resultSet.getString("technology_id") + "," + resultSet.getString("percentage");
+                return idNameStr;
+            }
+        };
+
+        List<String> resultSet = getNamedParameterJdbcTemplate().query(query.toString(), mapper);
+
+        for (int i = 0; i < resultSet.size(); i++) {
+            String[] temp = resultSet.get(i).split(",");
+            result.put(Integer.parseInt(temp[0].trim()), Double.parseDouble(temp[1].trim()));
+        }
+
+        return result;
     }
 }
