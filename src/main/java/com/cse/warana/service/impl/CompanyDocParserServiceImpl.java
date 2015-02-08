cae1e157af12a6639593ad36a9efe60b1a031d04
thilina123 <teez.mrt@gmail.com>
2015-02-08 21:35:02 +0530

    Issue sloving

--- src/main/java/com/cse/warana/service/impl/CompanyDocParserServiceImpl.java
+++ src/main/java/com/cse/warana/service/impl/CompanyDocParserServiceImpl.java
@@ -108,6 +108,6 @@ public class CompanyDocParserServiceImpl implements CompanyDocParserService {
                 break;
             }
         }
-        companyTechnologyService.storeCompanyTechnologies(list);
+//        companyTechnologyService.storeCompanyTechnologies(list);
     }
 }
